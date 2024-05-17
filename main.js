22f469d6ac1232db1494c72bdd9669880a0ce01b
yantowsky <it.yantowsky@gmail.com>
2024-05-17 19:00:00 +0300

    Renamed messages to console.log

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ let integer = +prompt("Введіть ціле число:");
 
 switch (integer) {
     case 1:
-        console.log(`${integer} - не є ні простим, ні складовим числом, тому що у нього тільки один дільник.`);
+        console.log(`${integer} - не є не простим, не складовим числом, тому що у нього тільки один дільник.`);
         break;
     default:
         for (i = 2; i <= integer; i++) {
             if (integer % i === 0 && i < integer) {
-                console.log(`${integer} - число не просте`);
+                console.log(`${integer} - складове число`);
                 break;
             } else if (i === integer) {
-                console.log(`${integer} - число просте`);
+                console.log(`${integer} - просте число`);
             }
         }
 }
